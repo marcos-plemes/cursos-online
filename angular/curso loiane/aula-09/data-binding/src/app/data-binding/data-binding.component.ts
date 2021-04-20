d85d8c5a49ea8572736d7627838a9baa4e8d3de4
Marcos Vinicius <mvplemes@gmail.com>
2021-04-20 14:57:21 -0300

    feito aula 09

---
+++ angular/curso loiane/aula-09/data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+export class DataBindingComponent implements OnInit {
+
+  url:string = 'http:loiane.com' 
+  cursoAngular: boolean = true;
+  urlImagem = 'https://placekitten.com/640/360';
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+
+
+}
