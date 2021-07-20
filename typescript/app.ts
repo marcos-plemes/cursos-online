f84505331384f4d6e242a2fa2ef89b7ec4694077
Marcos Vinicius <mvplemes@gmail.com>
2021-07-20 09:52:14 -0300

    jjj

--- typescript/app.ts
+++ typescript/app.ts
@@ -1,29 +1,14 @@
- let message: string = "Help me, Obi-Wan Kenobi. You're my only hope!";
- console.log(message);
+import {Spacecraft, Containership} from './base-ships'
+import {MillenniunmFalcon} from './starfighters'
 
- let episode: number = 4;
- console.log("This is episode " + episode);
- episode = episode +1;
- console.log("Next episode is " + episode);
-  
- let favoriteDroid
- favoriteDroid = 'BB-8'
- favoriteDroid = 10
- console.log("My favorite droid is " + favoriteDroid)
 
- let isEnoughToBeatMF = function (parsecs: number) : boolean {
-     return parsecs < 12;
- }
+let ship = new Spacecraft('hyperdrive')
+ship.jumpIntoHyperspace()
 
- let distance = 11;
- console.log(`Is ${distance} parsecs enough to beat Millenniun Falcon? ${isEnoughToBeatMF(distance) ? 'YES': 'NO'}`)
 
- let call = (name: string) => console.log(`Do you copy, ${name}?`)
- call('R2')
 
-function inc (speed: number, inc: number = 1) : number {
-    return speed + 1;
-}
+let falcon = new MillenniunmFalcon();
+falcon.jumpIntoHyperspace();
 
-console.log(`inc (5,1) = ${inc(5,1)}`)
-console.log(`inc (5) = ${inc(5)}`)
+let goodForTheJob = (ship: Containership) => ship.cargoContainers > 2;
+console.log(`Is falcon good for the job? ${goodForTheJob (falcon) ? 'yes' : 'no'}`)
